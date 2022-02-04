c39ee723e6e24a085077c41c5b7aa355d1f22b9a
Juan-Estevez <juank2001estevez@gmail.com>
2022-02-04 17:12:16 -0500

    Patient Service Creation

---
+++ src/app/patient/Patient.ts
@@ -0,0 +1,11 @@
+/**
+ * Se encarga de mapear al paciente.
+ */
+export class Patient {
+    idPatient:string="";
+    name:string="";
+    dateOfBirth:string="";
+    idType:string="";
+    eps:string="";
+    clinicHistory:string="";
+}
