ca857cdb05856db8b612f3f6343184ac53da15d8
Juan-Estevez <juank2001estevez@gmail.com>
2022-02-16 13:46:46 -0500

    Updating karma

--- src/app/patient/patient.component.spec.ts
+++ src/app/patient/patient.component.spec.ts
@@ -1,7 +1,40 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Patient } from './Patient';
 
 import { PatientComponent } from './patient.component';
+import { PatientService } from './patient.service';
 
+let httpClientSpy: { post: jasmine.Spy };
+let patientService: PatientService;
+
+beforeEach(() => {
+  httpClientSpy = jasmine.createSpyObj('HttpClient', ['post']);
+  patientService = new PatientService(httpClientSpy as any);
+});
+
+it('should return expected heroes (HttpClient called once)', () => {
+  const expectedPatient: Patient =  {
+    "idPatient": "3030100",
+    "name": "Mauricio Morales",
+    "dateOfBirth": "2000-10-11",
+    "idType": "CC",
+    "eps": "EPS Test",
+    "clinicHistory": "OK"
+  };
+
+  httpClientSpy.post.and.returnValue(expectAsync(expectedPatient));
+//  httpClientSpy.get.and.returnValue(asyncData(expectedHeroes));
+  patientService.create(expectedPatient).subscribe(
+    patient => expect(patient).toEqual(expectedPatient, 'expected patient'),
+    fail
+  );
+  expect(httpClientSpy.post.calls.count()).toBe(1, 'one call');
+});
+
+
+
+/*
 describe('PatientComponent', () => {
   let component: PatientComponent;
   let fixture: ComponentFixture<PatientComponent>;
@@ -23,3 +56,4 @@ describe('PatientComponent', () => {
     expect(component).toBeTruthy();
   });
 });
+*/
