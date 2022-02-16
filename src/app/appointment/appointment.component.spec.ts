962d418dc23660c38563eb323c656a61bcd76572
Juan-Estevez <juank2001estevez@gmail.com>
2022-02-16 17:09:26 -0500

    Updating unit tests

--- src/app/appointment/appointment.component.spec.ts
+++ src/app/appointment/appointment.component.spec.ts
@@ -1,25 +1,57 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Appointment } from "./Appointment";
+import { AppointmentService } from "./appointment.service";
 
-import { AppointmentComponent } from './appointment.component';
+let httpClientSpy: { post: jasmine.Spy, put: jasmine.Spy, delete: jasmine.Spy, get: jasmine.Spy };
+let appointmentService: AppointmentService;
 
 describe('AppointmentComponent', () => {
-  let component: AppointmentComponent;
-  let fixture: ComponentFixture<AppointmentComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ AppointmentComponent ]
-    })
-    .compileComponents();
-  });
+    beforeEach(() => {
+        httpClientSpy = jasmine.createSpyObj('HttpClient', ['post', 'put', 'delete', 'get']);
+        appointmentService = new AppointmentService(httpClientSpy as any);
+    });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(AppointmentComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    it('should return expected appointment to POST (HttpClient called once)', () => {
+        const expectedAppointment: Appointment = {
+            "idAppointment": 46,
+            "doctor": "101",
+            "patient": "2",
+            "date": "2022-02-18",
+            "hour": 9
+        };
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+        httpClientSpy.post.and.returnValue(expectAsync(expectedAppointment));
+        appointmentService.create(expectedAppointment)
+        expect(httpClientSpy.post.calls.count()).toBe(1, 'one call');
+    });
+
+    it('should return expected appointment to PUT (HttpClient called once)', () => {
+        const expectedAppointment: Appointment = {
+            "idAppointment": 46,
+            "doctor": "101",
+            "patient": "2",
+            "date": "2022-02-23",
+            "hour": 10
+        };
+
+        httpClientSpy.put.and.returnValue(expectAsync(expectedAppointment));
+        appointmentService.update(expectedAppointment)
+        expect(httpClientSpy.put.calls.count()).toBe(1, 'one call');
+    });
+
+    it('should return expected appointment to DELETE (HttpClient called once)', () => {
+        const idToDelete = 46;
+
+        httpClientSpy.delete.and.returnValue(expectAsync(idToDelete));
+        appointmentService.delete(idToDelete);
+        expect(httpClientSpy.delete.calls.count()).toBe(1, 'one call');
+    });
+
+    it('should return expected appointment to GET (HttpClient called once)', () => {
+        const idToGet = 2;
+
+        httpClientSpy.delete.and.returnValue(expectAsync(idToGet));
+        appointmentService.get(idToGet);
+        expect(httpClientSpy.get.calls.count()).toBe(1, 'one call');
+    });
 });
